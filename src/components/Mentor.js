18c6e6ba7fef7418de6f775e420119a0f9a0df5e
thedevBurhan <smdburhan25@gmail.com>
2023-05-17 18:37:05 +0530

    add note

--- src/components/Mentor.js
+++ src/components/Mentor.js
@@ -1,45 +1,55 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 import Base from "../Base/Base";
-import {useHistory} from "react-router-dom"
-const Mentor = ({mentor,setMentor}) => {
-    // console.log(mentor)
-const history=useHistory()
-   //  Delete functions
+import { useHistory } from "react-router-dom";
+const Mentor = ({ mentor, setMentor }) => {
+  // console.log(mentor)
+  const history = useHistory();
+  //  Delete functions
 
-   const deleteMenDetail=(menid)=>{
-      // console.log(studObj);
-      const remainingmentor=mentor.filter((mentor,idx)=>idx!==menid)
-      setMentor(remainingmentor);
-   
-   }
-    return (
-       <Base
-       title={"Mentor Dashboard"}
-       description={"This page content all mentor data"}
-       >
-            <div className="card-container">
-                {mentor.map((mentor,idx)=>(
-                 <div className="card" key={idx}>
-                    <div className="content">
-
-                 <h3 style={{color:"Red"}}>{mentor.name}</h3>
-                 <div className="value">
-                 <p><b>Batch:</b> {mentor.batch}</p>
-                 <p><b>Gender:</b> {mentor.gender}</p>
-                 <p><b>Experience in years: </b>{mentor.experience}</p>
-                 </div>
-                 
-                 </div>
-                 <div className="control">
-                    <button onClick={()=>history.push(`/edits/${idx}`)}>Edit</button>{" "}
-                    <button onClick={()=>deleteMenDetail(idx)}>Delete</button>
-                 </div>
-             </div>))}
-               
+  const deleteMenDetail = (menid) => {
+    // console.log(studObj);
+    const remainingmentor = mentor.filter((mentor, idx) => idx !== menid);
+    setMentor(remainingmentor);
+  };
+  return (
+    <Base
+      title={"Mentor Dashboard"}
+      description={"This page content all mentor data"}
+    >
+      <p>
+        <b>Note:</b>If you make any <b>changes</b> go to Home/Dashboard and
+        refresh the tab to see changes...
+      </p>
+      <br />
+      <div className="card-container">
+        {mentor.map((mentor, idx) => (
+          <div className="card" key={idx}>
+            <div className="content">
+              <h3 style={{ color: "Red" }}>{mentor.name}</h3>
+              <div className="value">
+                <p>
+                  <b>Batch:</b> {mentor.batch}
+                </p>
+                <p>
+                  <b>Gender:</b> {mentor.gender}
+                </p>
+                <p>
+                  <b>Experience in years: </b>
+                  {mentor.experience}
+                </p>
+              </div>
             </div>
-       
-       </Base>
-    )      
-}
- 
+            <div className="control">
+              <button onClick={() => history.push(`/edits/${idx}`)}>
+                Edit
+              </button>{" "}
+              <button onClick={() => deleteMenDetail(idx)}>Delete</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </Base>
+  );
+};
+
 export default Mentor;
--- src/components/Students.js
+++ src/components/Students.js
@@ -1,47 +1,56 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 import Base from "../Base/Base";
-import {useHistory} from "react-router-dom"
+import { useHistory } from "react-router-dom";
 
+const Students = ({ students, setStudents }) => {
+  // console.log(students)
+  const history = useHistory();
+  //  Delete functions
 
-const Students = ({students,setStudents}) => {
-    // console.log(students)
-const history=useHistory()
-   //  Delete functions
-
-   const deleteStudDetail=(studid)=>{
-      // console.log(studObj);
-      const remainingStudents=students.filter((students,idx)=>idx!==studid)
-      setStudents(remainingStudents);
-   
-   }
-    return (
-       <Base
-       title={"Students Dashboard"}
-       description={"This page content all student data"}
-       >
-            <div className="card-container">
-                {students.map((students,idx)=>(
-                 <div className="card" key={idx}>
-                    <div className="content">
-
-                 <h3 style={{color:"Red"}}>{students.name}</h3>
-                 <div className="value">
-                 <p><b>Batch:</b> {students.batch}</p>
-                 <p><b>Gender:</b> {students.gender}</p>
-                 <p><b>Qualification: </b>{students.qualification}</p>
-                 </div>
-                 
-                 </div>
-                 <div className="control">
-                    <button onClick={()=>history.push(`/edit/${idx}`)}>Edit</button>{" "}
-                    <button onClick={()=>deleteStudDetail(idx)}>Delete</button>
-                 </div>
-             </div>))}
-               
+  const deleteStudDetail = (studid) => {
+    // console.log(studObj);
+    const remainingStudents = students.filter(
+      (students, idx) => idx !== studid
+    );
+    setStudents(remainingStudents);
+  };
+  return (
+    <Base
+      title={"Students Dashboard"}
+      description={"This page content all student data"}
+    >
+      <p>
+        <b>Note:</b>If you make any <b>changes</b> go to Home/Dashboard and
+        refresh the tab to see changes...
+      </p>
+      <br />
+      <div className="card-container">
+        {students.map((students, idx) => (
+          <div className="card" key={idx}>
+            <div className="content">
+              <h3 style={{ color: "Red" }}>{students.name}</h3>
+              <div className="value">
+                <p>
+                  <b>Batch:</b> {students.batch}
+                </p>
+                <p>
+                  <b>Gender:</b> {students.gender}
+                </p>
+                <p>
+                  <b>Qualification: </b>
+                  {students.qualification}
+                </p>
+              </div>
             </div>
-       
-       </Base>
-    )      
-}
- 
+            <div className="control">
+              <button onClick={() => history.push(`/edit/${idx}`)}>Edit</button>{" "}
+              <button onClick={() => deleteStudDetail(idx)}>Delete</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </Base>
+  );
+};
+
 export default Students;
